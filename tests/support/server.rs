307cc4a4dc8977e74fbc87ea171ba405cfe9f500
anproagile <anproagile@gmail.com>
2017-08-19 16:32:00 +1000

    make body return borrowed decoder

--- tests/support/server.rs
+++ tests/support/server.rs
@@ -23,6 +23,7 @@ pub struct Txn {
     pub read_timeout: Option<Duration>,
     pub response_timeout: Option<Duration>,
     pub write_timeout: Option<Duration>,
+    pub chunk_size: Option<usize>,
 }
 
 static DEFAULT_USER_AGENT: &'static str =
@@ -36,9 +37,6 @@ pub fn spawn(txns: Vec<Txn>) -> Server {
             let mut expected = txn.request;
             let reply = txn.response;
             let (mut socket, _addr) = listener.accept().unwrap();
-
-            socket.set_read_timeout(Some(Duration::from_secs(5))).unwrap();
-
             replace_expected_vars(&mut expected, addr.to_string().as_ref(), DEFAULT_USER_AGENT.as_ref());
 
             if let Some(dur) = txn.read_timeout {
@@ -46,12 +44,7 @@ pub fn spawn(txns: Vec<Txn>) -> Server {
             }
 
             let mut buf = [0; 4096];
-            assert!(buf.len() >= expected.len());
-
-            let mut n = 0;
-            while n < expected.len() {
-                n += socket.read(&mut buf).unwrap();
-            }
+            let n = socket.read(&mut buf).unwrap();
 
             match (::std::str::from_utf8(&expected), ::std::str::from_utf8(&buf[..n])) {
                 (Ok(expected), Ok(received)) => assert_eq!(expected, received),
@@ -63,10 +56,21 @@ pub fn spawn(txns: Vec<Txn>) -> Server {
             }
 
             if let Some(dur) = txn.write_timeout {
-                let headers_end = ::std::str::from_utf8(&reply).unwrap().find("\r\n\r\n").unwrap() + 4;
+                let headers_end = b"\r\n\r\n";
+                let headers_end = reply.windows(headers_end.len()).position(|w| w == headers_end).unwrap() + 4;
                 socket.write_all(&reply[..headers_end]).unwrap();
-                thread::park_timeout(dur);
-                socket.write_all(&reply[headers_end..]).unwrap();
+
+                let body = &reply[headers_end..];
+
+                if let Some(chunk_size) = txn.chunk_size {
+                    for content in body.chunks(chunk_size) {
+                        thread::park_timeout(dur);
+                        socket.write_all(&content).unwrap();
+                    }
+                } else {
+                    thread::park_timeout(dur);
+                    socket.write_all(&body).unwrap();
+                }
             } else {
                 socket.write_all(&reply).unwrap();
             }
